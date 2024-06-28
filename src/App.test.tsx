ac53992e1b3b7447c392ae4b61867974d9f2dfd6
ivanlie <ivan.lie@meditap.id>
2024-06-28 16:20:27 +0700

    setup vitest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,12 @@
-import '@testing-library/jest-dom'
+import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import App from './App'
 
 describe('App Content', () => {
-  it('should render a button', () => {
+  it('Should render a button', () => {
     render(<App />)
 
-    expect(screen.getByTestId('button-test')).toBeInTheDocument()
+    expect(screen.getByTestId('button-test'))
   })
 })
--- src/components/Box.test.tsx
+++ src/components/Box.test.tsx
@@ -1,12 +1,12 @@
-import '@testing-library/jest-dom'
+import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 
 import Box from './Box'
 
 describe('Box Component', () => {
-  it('should render a button', () => {
+  it('Should render a box', () => {
     render(<Box />)
 
-    expect(screen.getByTestId('box-test')).toBeInTheDocument()
+    expect(screen.getByTestId('box-test'))
   })
 })
