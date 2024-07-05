55718cb9a8c249c07df91467453e1e8b33b19f74
ivanlie <ivan.lie@meditap.id>
2024-07-05 13:35:15 +0700

    env api url

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <main>
       <Box>
+        <h1>Hello World!</h1>
+        <p>API URL: {process.env.VITE_API_URL}</p>
         <button data-testid="button-test">This is a button</button>
       </Box>
     </main>
